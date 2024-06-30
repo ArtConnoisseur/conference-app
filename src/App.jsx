7560d1e0f0d5ac24a013c6ad04abf563fd46c7d9
Rudraksh Srivsatva <rudraksh2805@gmail.com>
2024-06-30 20:34:08 +0530

    Removed some sensitive info

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,8 @@ import './App.css';
 import firebase from 'firebase/compat/app';
 import 'firebase/firestore';
 import { useRef } from 'react';
+import { firebaseConfig } from './private-info';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDufMvsrNh3nPGgZuXjb1KNriG32Rm8u1U",
-  authDomain: "conference-app-ab6e6.firebaseapp.com",
-  projectId: "conference-app-ab6e6",
-  storageBucket: "conference-app-ab6e6.appspot.com",
-  messagingSenderId: "195726448004",
-  appId: "1:195726448004:web:5a7a07a38339ef702839d1"
-};
 
 firebase.initializeApp(firebaseConfig);
 
@@ -53,17 +46,3 @@ function App() {
 }
 
 export default App
-
-
-// const iceServers = [
-//   { urls: "stun:stun.l.google.com:19302" },
-//   { urls: "stun:stun.l.google.com:5349" },
-//   { urls: "stun:stun1.l.google.com:3478" },
-//   { urls: "stun:stun1.l.google.com:5349" },
-//   { urls: "stun:stun2.l.google.com:19302" },
-//   { urls: "stun:stun2.l.google.com:5349" },
-//   { urls: "stun:stun3.l.google.com:3478" },
-//   { urls: "stun:stun3.l.google.com:5349" },
-//   { urls: "stun:stun4.l.google.com:19302" },
-//   { urls: "stun:stun4.l.google.com:5349" }
-// ];
